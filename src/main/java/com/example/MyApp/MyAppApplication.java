1e1b0089c8416686b8cfcd0f99389944a57e7f3e
pavlin-dimitrov <pavlin.k.dimitrov@gmail.com>
2022-12-12 16:45:47 +0200

    Flyway connected.

--- src/main/java/com/example/MyApp/MyAppApplication.java
+++ src/main/java/com/example/MyApp/MyAppApplication.java
@@ -2,8 +2,10 @@ package com.example.MyApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class MyAppApplication {
 
     public static void main(String[] args) {
